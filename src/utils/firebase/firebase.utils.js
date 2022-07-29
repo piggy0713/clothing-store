4d929a317790123e761c39af789422800438108f
Karida Chen <piggy.moomoo520@gmail.com>
2022-07-29 19:33:49 -0400

    chagne env

--- src/utils/firebase/firebase.utils.js
+++ src/utils/firebase/firebase.utils.js
@@ -22,13 +22,13 @@ import {
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyBkLDbAo6T3ep9raAcRf3WcVgrawRQP7pM",
-  authDomain: "crwn-clothing-db-3ef62.firebaseapp.com",
-  projectId: "crwn-clothing-db-3ef62",
-  storageBucket: "crwn-clothing-db-3ef62.appspot.com",
-  messagingSenderId: "456181002300",
-  appId: "1:456181002300:web:bbe97c0cc3082a32581339",
-  measurementId: "G-3J1LXPKL6H",
+  apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+  authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.REACT_APP_FIREBASE_APP_ID,
+  measurementId: process.env.REACT_APP_FIREBASE_MEASUREMENT_ID,
 };
 
 // Initialize Firebase
