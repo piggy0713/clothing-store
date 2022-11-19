0d110654e720bd50d381a2e2eb85268fcc5749bd
Karida Chen <piggy.moomoo520@gmail.com>
2022-11-19 16:08:26 -0500

    remove react snap

--- src/routes/home/home.component.jsx
+++ src/routes/home/home.component.jsx
@@ -1,14 +1,22 @@
 import { Outlet } from "react-router-dom";
 import Directory from "../../components/directory/directory.component";
-import SEO from "../../components/SEO";
+import { Helmet } from "react-helmet";
 
 const Home = () => {
   return (
     <>
-      <SEO
-        title="Home - KM Clothing"
-        description="Clothing store for all you needs"
-      />
+      <Helmet>
+        <title>Home</title>
+        <link rel="canonical" href="clothing-store-lovat.vercel.app" />
+        <meta name="description" content="Home page" />
+        <meta property="og:title" content="Home" />
+        <meta property="og:description" content="Home page" />
+        <meta property="og:url" content="clothing-store-lovat.vercel.app" />
+        <meta
+          property="og:image"
+          content="clothing-store-lovat.vercel.app/km.png"
+        />
+      </Helmet>
       <Directory />
       <Outlet />
     </>
